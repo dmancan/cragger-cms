932d2e242f634fcdb5f31d3bdba6c614e7507b47
Darrell Houle <darrell@ethicalux.com>
2017-04-27 00:37:57 -0600

    pushing up parse

--- index.js
+++ index.js
@@ -1,9 +1,51 @@
 var express = require('express');
+var ParseServer = require('parse-server').ParseServer;
+var path = require('path');
+
+
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+/* Parse */
 
-app.use(express.static(__dirname + '/public'));
+var databaseUri = process.env.DATABASE_URI || process.env.MONGODB_URI;
+
+if (!databaseUri) {
+  console.log('DATABASE_URI not specified, falling back to localhost.');
+}
+
+//mongodb://localhost:27017/dev
+var api = new ParseServer({
+  databaseURI: databaseUri || 'mongodb://heroku_6gr5p5cs:1p4idvipdcg24ab4qnmou56aa8@ds121171.mlab.com:21171/heroku_6gr5p5cs',
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+  appId: process.env.APP_ID || 'myAppId',
+  masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
+  serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
+  liveQuery: {
+    classNames: ["Posts", "Comments"] // List of classes to support for query subscriptions
+  }
+});
+
+// Serve the Parse API on the /parse URL prefix
+var mountPath = process.env.PARSE_MOUNT || '/parse';
+app.use(mountPath, api);
+
+
+var port = process.env.PARSE_PORT || 1337;
+var httpServer = require('http').createServer(app);
+httpServer.listen(port, function() {
+    console.log('parse-server-example running on port ' + port + '.');
+});
+
+// This will enable the Live Query real-time server
+ParseServer.createLiveQueryServer(httpServer);
+
+/* end Parse */
+
+
+
+app.set('webport', (process.env.PORT || 3000));
+
+app.use('/public', express.static(__dirname + '/public'));
 
 // views is directory for all template files
 app.set('views', __dirname + '/views');
@@ -13,8 +55,12 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+// There will be a test page available on the /test path of your server url
+// Remove this before launching your app
+app.get('/test', function(req, res) {
+  res.sendFile(path.join(__dirname, '/public/test.html'));
 });
 
-
+app.listen(app.get('webport'), function() {
+  console.log('Node app is running on port', app.get('webport'));
+});
